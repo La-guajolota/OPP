868aa41357349127d0a60207b957195208811b0c
Adrian <adriansilpa@gmail.com>
2024-06-20 00:58:33 -0600

    Ejercicio JACOB

--- JACOB_ejerccios/resistencias.cpp
+++ JACOB_ejerccios/resistencias.cpp
@@ -3,12 +3,19 @@ using namespace std;
 
 // Variables globales
 int op; // OP -> Opcion escogida
+int Rn; // Cantidad de resistencias
+
+float Req; // Resistencia equivalente
+float aux; // Variable auxiliar para calculos
 
 int main()
 {
 
     do
     {
+        // Reinicializamos para nuevas operaciones
+        Req = 0;
+
         // El usuario ecoge opcion
         cout << "ESCOGE UNA OPCION (1,2 o 3): " << endl;
         cin >> op;
@@ -26,10 +33,54 @@ int main()
         if (op == 1) // Circuito serie
         {
             cout << "Escogiste Serie: " << endl;
+            cout << "Cuantas resistencias en serie tienes? 1 a 5" << endl;
+            cin >> Rn;
+
+            // Checamos cuantas resitencias pidio el usuario
+            if (Rn >= 1 || Rn <= 5)
+            {
+                for (int i = 0; i < Rn; i++)
+                {
+                    cout << "Cuanto vale la resistencia numero: " << i + 1 << " ?" << endl;
+                    cin >> aux;
+
+                    // Resistencia equivalente es la suma de las resistencias
+                    Req += aux;
+                }
+
+                cout << "Resistenci equivalente  en serie es: " << Req << endl;
+            }
+            else
+            {
+                cout << "ERROR. No es valido más de 5 resistencia o menos de 1" << endl;
+            }
         }
         else if (op == 2) // Circuito paralelo
         {
             cout << "Escogiste Paralelo: " << endl;
+            cout << "Escogiste Serie: " << endl;
+            cout << "Cuantas resistencias en serie tienes? 1 a 5" << endl;
+            cin >> Rn;
+
+            // Checamos cuantas resitencias pidio el usuario
+            if (Rn >= 1 || Rn <= 5)
+            {
+                for (int i = 0; i < Rn; i++)
+                {
+                    cout << "Cuanto vale la resistencia numero: " << i + 1 << " ?" << endl;
+                    cin >> aux;
+
+                    // Resistencia equivalente es la suma de inversos de las resistencias
+                    Req += (1 / aux); // ! Req += 1.0 / aux;
+                }
+
+                // Inverso de la sumatoria de inversos de las resitencias
+                cout << "Resistenci equivalente  en serie es: " << (1.0 / Req) << endl;
+            }
+            else
+            {
+                cout << "ERROR. No es valido más de 5 resistencia o menos de 1" << endl;
+            }
         }
         else if (op == 3) // Salida
         {
